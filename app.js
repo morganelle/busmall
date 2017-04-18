b2c453fb5bddc652cd7894bcd85c90711d66c982
Morgan Nomura <morganelle@gmail.com>
2017-04-18 09:51:54 -0700

    refactored app.js

--- app.js
+++ app.js
@@ -2,14 +2,13 @@
 
 // Global variables
 var chosenProducts = []; // variable to hold the last chosen products
-var displayNumber = 3; // how many products to show at one time
+var products = []; // stores Product instances
 var roundsCount = 25; // how many times the user will get a choice of products
 var totalClicks = 0;
 
 // Global DOM variables
-var divOneEl = document.getElementById('product1');
-var divTwoEl = document.getElementById('product2');
-var divThreeEl = document.getElementById('product3');
+var divEls = [];
+var sectionEl = document.getElementById('user-choice');
 var resultsSectionEl = document.getElementById('results');
 var context = document.getElementById('results-chart').getContext('2d');
 
@@ -20,12 +19,11 @@ var chartLabels = [];
 // Object constructor for products
 function Product(filename, id, displayName) {
   this.filename = filename;
-  this.filepath = 'images/' + filename;
   this.id = id;
   this.displayName = displayName;
   this.displayCount = 0;
   this.clickCount = 0;
-  // localStorage.setItem(this.id, this.clickCount); // currently recreates localStorage item every time this runs! REFACTOR
+  products.push(this);
 }
 
 Product.prototype.createImage = function(divDOM) {
@@ -33,7 +31,7 @@ Product.prototype.createImage = function(divDOM) {
   var productImageEl = document.createElement('img');
   productImageEl.setAttribute('id', this.id);
   productImageEl.setAttribute('class', 'product-image');
-  productImageEl.setAttribute('src', this.filepath);
+  productImageEl.setAttribute('src', 'images/' + this.filename);
   divDOM.appendChild(productImageEl);
 };
 Product.prototype.removeImage = function() {
@@ -43,7 +41,6 @@ Product.prototype.removeImage = function() {
 };
 Product.prototype.incrementStorage = function() {
   var localClick = JSON.parse(localStorage.getItem(this.id));
-  console.log(localClick);
   localClick++;
   console.log(localClick);
   localStorage.setItem(this.id, localClick);
@@ -54,25 +51,22 @@ Product.prototype.beginLocalStorage = function() {
   }
   else {
     localStorage.setItem(this.id, this.clickCount);
-    console.log(this.id + ' created in exists localStorage');
+    console.log(this.id + ' created in localStorage');
   }
 };
-Product.prototype.productClick = function(event) {
+Product.prototype.productClick = function() {
   this.beginLocalStorage();
   this.clickCount++;
   this.incrementStorage();
   totalClicks++;
-  // clearObjects();
   // Chooses and displays new products while totalClicks is less than roundsCount
   while (totalClicks < roundsCount) {
-    clearObjects();
-    chooseProduct();
-    displayObjects();
+    clearObjects(3);
+    runTest(3);
     break;
   }
   if (totalClicks === roundsCount) {
     displayTotals(products);
-    console.log(localStorage);
     populateChartLabels(products);
     populateChartData(products);
     // chart start
@@ -105,26 +99,26 @@ Product.prototype.productClick = function(event) {
 
 // Product template
 // var name = new Product('filename', 'id', 'description');
-var bag = new Product('bag.jpg', 'bag', 'wheely bag');
-var banana = new Product('banana.jpg', 'banana', 'nanner slicer');
-var bathroom = new Product('bathroom.jpg', 'bathroom', 'tablet \'n TP');
-var boots = new Product('boots.jpg', 'boots', 'toe-free boots');
-var breakfast = new Product('breakfast.jpg', 'breakfast', 'breakfast machine');
-var bubblegum = new Product('bubblegum.jpg', 'bubblegum', 'meaty chewing gum');
-var chair = new Product('chair.jpg', 'chair', 'chair with domed seat');
-var cthulhu = new Product('cthulhu.jpg', 'cthulhu', 'tiny cthulhu');
-var dogDuck = new Product('dog-duck.jpg', 'dog-duck', 'pup quackers');
-var dragon = new Product('dragon.jpg', 'dragon', 'exotic meats: dragon');
-var pen = new Product('pen.jpg', 'pen', 'cutlery pens');
-var petSweep = new Product('pet-sweep.jpg', 'pet-sweep', 'sweeper slippers for pets');
-var scissors = new Product('scissors.jpg', 'scissors', 'pizza sheers');
-var shark = new Product('shark.jpg', 'shark', 'shark bag');
-var sweep = new Product('sweep.png', 'sweep', 'sweeper onesie');
-var tauntaun = new Product('tauntaun.jpg', 'tauntaun', 'tauntaun bag');
-var unicorn = new Product('unicorn.jpg', 'unicorn', 'exotic meats: unicorn');
-var usb = new Product('usb.gif', 'usb', 'living USB');
-var waterCan = new Product('water-can.jpg', 'water-can', 'watering made difficult');
-var wineGlass = new Product('wine-glass.jpg', 'wine-glass', 'teetotaler glass');
+new Product('bag.jpg', 'bag', 'wheely bag');
+new Product('banana.jpg', 'banana', 'nanner slicer');
+new Product('bathroom.jpg', 'bathroom', 'tablet \'n TP');
+new Product('boots.jpg', 'boots', 'toe-free boots');
+new Product('breakfast.jpg', 'breakfast', 'breakfast machine');
+new Product('bubblegum.jpg', 'bubblegum', 'meaty chewing gum');
+new Product('chair.jpg', 'chair', 'chair with domed seat');
+new Product('cthulhu.jpg', 'cthulhu', 'tiny cthulhu');
+new Product('dog-duck.jpg', 'dog-duck', 'pup quackers');
+new Product('dragon.jpg', 'dragon', 'exotic meats: dragon');
+new Product('pen.jpg', 'pen', 'cutlery pens');
+new Product('pet-sweep.jpg', 'pet-sweep', 'sweeper slippers for pets');
+new Product('scissors.jpg', 'scissors', 'pizza sheers');
+new Product('shark.jpg', 'shark', 'shark bag');
+new Product('sweep.png', 'sweep', 'sweeper onesie');
+new Product('tauntaun.jpg', 'tauntaun', 'tauntaun bag');
+new Product('unicorn.jpg', 'unicorn', 'exotic meats: unicorn');
+new Product('usb.gif', 'usb', 'living USB');
+new Product('water-can.jpg', 'water-can', 'watering made difficult');
+new Product('wine-glass.jpg', 'wine-glass', 'teetotaler glass');
 
 // Function that chooses random number
 function random() {
@@ -132,38 +126,43 @@ function random() {
 }
 
 // Function that chooses 3 random numbers without repeats
-function chooseProduct() {
+function chooseProduct(count) {
   var product;
-  for (var i = 0; i < 3; i++) {
+  for (var i = 0; i < count; i++) {
     do {
       product = random();
     }
     while (chosenProducts.includes(product));
     chosenProducts.push(product);
   }
-  if (chosenProducts.length > 3) {
-    chosenProducts.splice(0,3);
+  if (chosenProducts.length > count) {
+    chosenProducts.splice(0,count);
+  }
+}
+
+function createDivs(count) {
+  for (var k = 0; k < count; k++) {
+    var div = document.createElement('div');
+    div.setAttribute('id', 'product' + k);
+    div.setAttribute('class', 'product');
+    sectionEl.appendChild(div);
+    divEls.push(div);
   }
 }
 
 // Function that displays objects in DOM
-function displayObjects() {
-  products[chosenProducts[0]].createImage(divOneEl);
-  products[chosenProducts[1]].createImage(divTwoEl);
-  products[chosenProducts[2]].createImage(divThreeEl);
+function displayObjects(count) {
+  for (var j = 0; j < count; j++) {
+    products[chosenProducts[j]].createImage(divEls[j]);
+  }
 }
 
 // Function that removes imgs from DOM
-function clearObjects() {
-  products[chosenProducts[0]].removeImage();
-  products[chosenProducts[1]].removeImage();
-  products[chosenProducts[2]].removeImage();
+function clearObjects(count) {
+  for (var j = 0; j < count; j++) {
+    products[chosenProducts[j]].removeImage();
+  }
 }
-
-// Adds event listeners to three divs in user-test.html
-divOneEl.addEventListener('click', function() {products[chosenProducts[0]].productClick(event);}, false);
-divTwoEl.addEventListener('click', function() {products[chosenProducts[1]].productClick(event);}, false);
-divThreeEl.addEventListener('click', function() {products[chosenProducts[2]].productClick(event);}, false);
 
 // After test has run, show images and their totals
 function displayTotals(productArray) {
@@ -171,7 +170,7 @@ function displayTotals(productArray) {
     var divEl = document.createElement('div');
     divEl.setAttribute('class', 'result');
     var imgEl = document.createElement('img');
-    imgEl.setAttribute('src', productArray[index].filepath);
+    imgEl.setAttribute('src', 'images/' + productArray[index].filename);
     var paraEl = document.createElement('p');
     paraEl.textContent = ('Clicked: ' + productArray[index].clickCount + ' / Displayed: ' + productArray[index].displayCount);
     divEl.appendChild(paraEl);
@@ -180,25 +179,10 @@ function displayTotals(productArray) {
   }
 }
 
-// // Create arrays for chart.js
-// function populateChartData(productArray) {
-//   for (var i = 0; i < productArray.length; i++) {
-//     var chartDatum = (productArray[i].clickCount);
-//     chartData.push(chartDatum);
-//   }
-// }
-// function populateChartLabels(productArray) {
-//   for (var i = 0; i < productArray.length; i++) {
-//     var chartDatum = productArray[i].id;
-//     chartLabels.push(chartDatum);
-//   }
-// }
-
 // Create arrays for chart.js
 function populateChartData(productArray) {
-  for (var i = 0; i < localStorage.length; i++) {
+  for (var i = 0; i < productArray.length; i++) {
     var chartDatum = JSON.parse(localStorage.getItem(productArray[i].id));
-    console.log(chartDatum);
     chartData.push(chartDatum);
   }
 }
@@ -209,12 +193,18 @@ function populateChartLabels(productArray) {
   }
 }
 
-// Array of all products
-var products = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass]; // all products
-
-function runTest() {
-  chooseProduct();
-  displayObjects();
+function beginTest(count) {
+  localStorage.clear();
+  createDivs(count);
+  divEls[0].addEventListener('click', function() {products[chosenProducts[0]].productClick(event);}, false);
+  divEls[1].addEventListener('click', function() {products[chosenProducts[1]].productClick(event);}, false);
+  divEls[2].addEventListener('click', function() {products[chosenProducts[2]].productClick(event);}, false);
+  runTest(count);
 }
 
-runTest();
+function runTest(count) {
+  chooseProduct(count);
+  displayObjects(count);
+}
+
+beginTest(3);
